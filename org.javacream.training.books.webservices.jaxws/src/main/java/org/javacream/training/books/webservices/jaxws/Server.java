6090e32e919797978131fadbb0825f77d19b91e4
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-08-07 10:12:59 +0200

    implement StoreWebService

--- org.javacream.training.books.webservices.jaxws/src/main/java/org/javacream/training/books/webservices/jaxws/Server.java
+++ org.javacream.training.books.webservices.jaxws/src/main/java/org/javacream/training/books/webservices/jaxws/Server.java
@@ -3,6 +3,7 @@ package org.javacream.training.books.webservices.jaxws;
 import javax.xml.ws.Endpoint;
 
 import org.javacream.training.isbngenerator.webservices.jaxws.IsbnGeneratorWebService;
+import org.javacream.training.storeservice.webservices.jaxws.StoreWebService;
 import org.javacream.util.Context;
 
 public class Server {
@@ -11,7 +12,10 @@ public class Server {
 		System.out.println("Starting services...");
 		IsbnGeneratorWebService isbnGeneratorWebService = new IsbnGeneratorWebService();
 		isbnGeneratorWebService.setIsbnGenerator(Context.getKeyGenerator());
+		StoreWebService storeWebService = new StoreWebService();
+		storeWebService.setStoreService(Context.getStoreService());
 		Endpoint.publish("http://localhost:8888/isbnGenerator", isbnGeneratorWebService);
+		Endpoint.publish("http://localhost:8888/storeService", storeWebService);
 		System.out.println("Starting services done!");
 	}
 
---
+++ org.javacream.training.books.webservices.jaxws/src/main/java/org/javacream/training/storeservice/webservices/jaxws/StoreWebService.java
@@ -0,0 +1,24 @@
+package org.javacream.training.storeservice.webservices.jaxws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+
+import org.javacream.store.StoreService;
+
+@WebService
+public class StoreWebService {
+	private StoreService storeService;
+	@WebMethod(exclude=false, operationName="GetStock")	
+	@WebResult(name="Stock") public int getStock(@WebParam(name= "Category") String category, @WebParam(name= "ItemId") String id) {
+		return storeService.getStock(category, id);
+	}
+
+	@WebMethod(exclude=true)
+	public void setStoreService(StoreService storeService) {
+		this.storeService = storeService;
+	}
+	
+	
+}
