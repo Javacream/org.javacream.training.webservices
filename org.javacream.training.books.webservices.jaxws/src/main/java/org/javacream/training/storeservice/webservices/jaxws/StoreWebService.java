8bce98ae6ab29c83913cdefd63649a485f1d0c80
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-08-07 14:18:02 +0200

    implement batch mode for storeservice

--- org.javacream.training.books.webservices.jaxws/src/main/java/org/javacream/training/storeservice/webservices/jaxws/StoreWebService.java
+++ org.javacream.training.books.webservices.jaxws/src/main/java/org/javacream/training/storeservice/webservices/jaxws/StoreWebService.java
@@ -1,5 +1,8 @@
 package org.javacream.training.storeservice.webservices.jaxws;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
@@ -8,11 +11,15 @@ import javax.jws.WebService;
 import org.javacream.store.api.StoreService;
 
 @WebService
-public class StoreWebService {
+public class StoreWebService{
 	private StoreService storeService;
 	@WebMethod(exclude=false, operationName="GetStock")	
-	@WebResult(name="Stock") public int getStock(@WebParam(name= "Category") String category, @WebParam(name= "ItemId") String id) {
-		return storeService.getStock(category, id);
+	@WebResult(name="Stock") public List<Integer> getStock(@WebParam(name= "Category") String category, @WebParam(name= "ItemIds") List<String> ids) {
+		List<Integer> result = new ArrayList<>();
+		for (int i = 0; i < ids.size(); i++) {
+			result.add(storeService.getStock(category, ids.get(i)));
+		}
+		return result;
 	}
 
 	@WebMethod(exclude=true)
