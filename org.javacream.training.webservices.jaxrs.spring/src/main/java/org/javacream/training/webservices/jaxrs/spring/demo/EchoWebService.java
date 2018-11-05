b91805a4cc06bec3bb3f76384d878f637c7ffe75
Rainer Sawitzki <training@rainer-sawitzki.de>
2018-11-05 21:37:34 +0100

    add spring jaxrs examples with and without maven

---
+++ org.javacream.training.webservices.jaxrs.spring/src/main/java/org/javacream/training/webservices/jaxrs/spring/demo/EchoWebService.java
@@ -0,0 +1,16 @@
+package org.javacream.training.webservices.jaxrs.spring.demo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("echo")
+public class EchoWebService {
+
+	@GET @Path("ping")
+	public String ping() {
+		return "pong";
+	}
+}
