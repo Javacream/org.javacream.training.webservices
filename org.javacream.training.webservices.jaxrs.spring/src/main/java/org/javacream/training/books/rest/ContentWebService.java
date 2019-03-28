d701842373e550315ff1c4f4eb7dbe0db8152228
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-03-28 16:58:07 +0100

    add ContentWebService

---
+++ org.javacream.training.webservices.jaxrs.spring/src/main/java/org/javacream/training/books/rest/ContentWebService.java
@@ -0,0 +1,26 @@
+package org.javacream.training.books.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import org.javacream.application.Context;
+import org.javacream.books.warehouse.api.BooksService;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("content")
+public class ContentWebService {
+
+	private BooksService booksService = Context.getBooksService();
+
+	@GET @Path("{isbn}")
+	public String getContent(@PathParam("isbn") String isbn) {
+		try {
+			return new String(booksService.findBookByIsbn(isbn).getContent());
+		} catch (Exception e) {
+			throw new NotFoundException();
+		}
+	}
+}
