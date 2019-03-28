252fa6389693b55418ce061d52dd35ffe357ffa5
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-03-28 14:07:17 +0100

    update StoreService, add setStock and remove operations

--- org.javacream.training.books/src/org/javacream/store/api/StoreService.java
+++ org.javacream.training.books/src/org/javacream/store/api/StoreService.java
@@ -3,4 +3,9 @@ package org.javacream.store.api;
 public interface StoreService {
 
 	int getStock(String category, String id);
+	void setStock(String category, String id, int stock);
+	void remove(String category, String id);
+	void remove(String category);
+	void remove();
+	
 }
--- org.javacream.training.books/src/org/javacream/store/impl/StoreServiceImpl.java
+++ org.javacream.training.books/src/org/javacream/store/impl/StoreServiceImpl.java
@@ -1,11 +1,53 @@
 package org.javacream.store.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.javacream.store.api.StoreService;
 
 public class StoreServiceImpl implements StoreService{
 
+	private Map<String, Map<String, Integer>> stockMap;
+	
+	{
+		stockMap = new HashMap<>();
+	}
 	public int getStock(String category, String id) {
-		return 42;
+		Map<String, Integer> ids = stockMap.get(category);
+		if (ids == null) {
+			return 0;
+		}
+		Integer stock = ids.get(id);
+		if (id == null) {
+			return 0;
+		}
+		return stock;
+	}
+	@Override
+	public void setStock(String category, String id, int stock) {
+		Map<String, Integer> ids = stockMap.get(category);
+		if (ids == null) {
+			ids = new HashMap<>();
+			stockMap.put(category, ids);
+		}
+		ids.put(id, stock);
+		
+	}
+	@Override
+	public void remove(String category, String id) {
+		Map<String, Integer> ids = stockMap.get(category);
+		if (ids != null) {
+			stockMap.remove(id);
+		}
+	}
+	@Override
+	public void remove(String category) {
+		stockMap.remove(category);
+	}
+	@Override
+	public void remove() {
+		stockMap.clear();
+		
 	}
 
 }
