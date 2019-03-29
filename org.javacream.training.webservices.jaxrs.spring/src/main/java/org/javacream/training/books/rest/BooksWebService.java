c2808b165ffdfcabd1e3fb79560c189ccc716374
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-03-29 10:46:21 +0100

    fix Http Status Codes in BooksWebService

--- org.javacream.training.webservices.jaxrs.spring/src/main/java/org/javacream/training/books/rest/BooksWebService.java
+++ org.javacream.training.webservices.jaxrs.spring/src/main/java/org/javacream/training/books/rest/BooksWebService.java
@@ -12,6 +12,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import org.javacream.application.Context;
@@ -25,70 +26,66 @@ import io.swagger.annotations.Api;
 @Api
 public class BooksWebService {
 	private BooksService booksService = Context.getBooksService();
-	
-	
-	@GET @Path("{isbn}") @Produces({MediaType.TEXT_PLAIN, "text/simple"})
-	public WebBook findBookByIsbn(@PathParam ("isbn")String isbn){
-		try{
+
+	@GET
+	@Path("{isbn}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public WebBook findBookByIsbn(@PathParam("isbn") String isbn) {
+		try {
 			return assemble(booksService.findBookByIsbn(isbn));
-		}
-		catch(BookException be){
+		} catch (BookException be) {
 			throw new NotFoundException(be);
 		}
 	}
 
-	@GET @Path("{isbn}") @Produces(MediaType.APPLICATION_JSON)
-	public WebBook findBookByIsbnAsJson(@PathParam ("isbn")String isbn){
-		try{
-			return assemble(booksService.findBookByIsbn(isbn));
-		}
-		catch(BookException be){
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<WebBook> findAll() {
+		try {
+			return booksService.findAllBooks().stream().map((book) -> assemble(book)).collect(Collectors.toList());
+		} catch (Exception be) {
 			throw new NotFoundException(be);
 		}
 	}
 
-	
-	@GET  @Produces(MediaType.APPLICATION_JSON)
-	public Collection<WebBook> findAll(){
-		return booksService.findAllBooks().stream().map((book) -> assemble(book)).collect(Collectors.toList());
-	}
-
-	@POST @Path("{title}") @Produces(MediaType.TEXT_PLAIN)
-	public String newBook(@PathParam ("title")String title){
-		try{
+	@POST
+	@Path("{title}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String newBook(@PathParam("title") String title) {
+		try {
 			return booksService.newBook(title);
-		}
-		catch(BookException be){
-			throw new RuntimeException(be);
-		}
-	}
-	
-	@DELETE @Path("{isbn}")
-	public void deleteBookByIsbn(@PathParam ("isbn")String isbn){
-		try{
-			booksService.deleteBookByIsbn(isbn);
-		}
-		catch(BookException be){
-			throw new RuntimeException(be);
+		} catch (BookException be) {
+			throw new WebApplicationException(422);
 		}
 	}
 
-	@PUT @Consumes(MediaType.APPLICATION_JSON) @Produces(MediaType.APPLICATION_JSON) @Path("{isbn}") 
-	public WebBook updateBook(@PathParam ("isbn") String isbn, WebBook toUpdate){
-		try{
+	@DELETE
+	@Path("{isbn}")
+	public void deleteBookByIsbn(@PathParam("isbn") String isbn) {
+		try {
+			booksService.deleteBookByIsbn(isbn);
+		} catch (BookException be) {
+			throw new WebApplicationException(409);
+		}
+	}
+
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("{isbn}")
+	public WebBook updateBook(@PathParam("isbn") String isbn, WebBook toUpdate) {
+		try {
 			Book book = booksService.findBookByIsbn(isbn);
 			book.setTitle(toUpdate.getTitle());
 			book.setPrice(toUpdate.getPrice());
 			return assemble(booksService.updateBook(book));
-		}
-		catch(BookException be){
-			throw new RuntimeException(be);
+		} catch (BookException be) {
+			throw new WebApplicationException(422);
 		}
 	}
 
-	
 	private WebBook assemble(Book book) {
 		return new WebBook(book.getIsbn(), book.getTitle(), book.getPrice(), book.isAvailable());
 	}
-	
+
 }
