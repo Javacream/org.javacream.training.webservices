f735cedac329ee3a12c1d4c58e3db0c3c7fb33c9
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-10-21 16:26:43 +0200

    add StoreServiceWebService

--- org.javacream.training.webservices/curlscripts/simple-curl.bat
+++ org.javacream.training.webservices/curlscripts/simple-curl.bat
@@ -11,3 +11,9 @@ Paramaterübergabe
 2. curl -verbose -X GET -H "Accept: text/plain" http://localhost:8080/api/echo?message=hello (Query-Parameter)
 3. curl -verbose -X GET -H "Accept: text/plain"  -H "message: Hello" http://localhost:8080/api/echo/header (Header-Parameter)
 4. curl -verbose -d "Hello" -X GET -H "Accept: text/plain" http://localhost:8080/api/echo (Body)
+
+
+curl -X GET http://localhost:8080/api/store/books/ISBN1
+curl -X GET http://localhost:8080/api/store/dvd/StarTrek
+curl -X GET "http://localhost:8080/api/store/query?category=dvd&item=StarTrek"
+curl -X GET -H "CATEGORY: dvd" -H "ID: StarTrek" "http://localhost:8080/api/store/header"
---
+++ org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/store/StoreWebService.java
@@ -0,0 +1,59 @@
+package org.javacream.training.webservices.store;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+
+import org.javacream.store.StoreService;
+import org.javacream.util.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("api")
+public class StoreWebService {
+
+	private StoreService storeService = new LoggingStoreService(ApplicationContext.getStoreService());
+
+
+	@GET
+	@Path("store/{c}/{i}")
+	public int getStockWithPath(@PathParam("c") String cat, @PathParam("i") String id) {
+		System.out.println("getStockWithPath");
+		return storeService.getStock(cat, id);
+	}
+
+	@GET
+	@Path("store/query")
+	public int getStockWithQuery(@QueryParam("category") String cat, @QueryParam("item") String id) {
+		System.out.println("getStockWithQuery");
+		return storeService.getStock(cat, id);
+	}
+
+	@GET
+	@Path("store/header")
+	public int getStockWithHeader(@HeaderParam("CATEGORY") String cat, @HeaderParam("ID") String id) {
+		System.out.println("getStockWithHeader");
+		return storeService.getStock(cat, id);
+	}
+
+	@GET
+	@Path("store/{c}")
+	public int getStockWithPathAndHeader(@PathParam("c") String cat, @HeaderParam("ID") String id) {
+		System.out.println("getStockWithPathAndHeader");
+		return storeService.getStock(cat, id);
+	}
+
+
+	private class LoggingStoreService implements StoreService{
+		private StoreService storeService;
+		public int getStock(String category, String item) {
+			System.out.println("rtrieving stock for category=" + category + ", item=" + item);
+			return storeService.getStock(category, item);
+		}
+		LoggingStoreService(StoreService storeService){
+			this.storeService = storeService;
+		}
+	}
+}
