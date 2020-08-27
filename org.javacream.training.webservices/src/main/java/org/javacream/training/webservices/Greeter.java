264641d0f5e417bb6fe51436025f482d0889a011
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-08-27 14:05:32 +0200

    implement greeter webservice

---
+++ org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/Greeter.java
@@ -0,0 +1,30 @@
+package org.javacream.training.webservices;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("api/greeter")
+public class Greeter {
+
+	//http://localhost:8080/api/greeter/Sawitzki
+	@Path("{name}")
+	@GET
+	@Produces("text/plain")
+	public String greetWithPathParam(@PathParam("name") String personName) {
+		return "Hello " + personName;
+	}
+
+	//http://localhost:8080/api/greeter?name=Sawitzki
+	@GET
+	@Produces("text/plain")
+	public String greetWithQueryParam(@QueryParam("name") String personName) {
+		return "Hello " + personName;
+	}
+
+}
