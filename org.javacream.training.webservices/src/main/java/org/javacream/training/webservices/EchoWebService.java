ec17e616332149c8b7abaa7a0126ecd1d7345c9d
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-20 14:15:45 +0200

    extend EchoWebService

--- org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/EchoWebService.java
+++ org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/EchoWebService.java
@@ -1,13 +1,21 @@
 package org.javacream.training.webservices;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoWebService {
 
-	@GetMapping(path = "ping")
+	@GetMapping(path = "ping", produces = MediaType.TEXT_PLAIN_VALUE)
 	public String ping() {
 		return "pong";
+	}
+	
+	@GetMapping(path="echo/{m}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String echo(@PathVariable("m") String message) {
+		System.out.println("receiving message "+ message);
+		return message;
 	}
 }
