be318a738eb669e30479c16a161faebc2359edee
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-20 15:13:36 +0200

    implement StoreWebService

--- org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/store/StoreWebService.java
+++ org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/store/StoreWebService.java
@@ -2,17 +2,20 @@ package org.javacream.training.webservices.store;
 
 import org.javacream.store.StoreService;
 import org.javacream.util.ApplicationContext;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StoreWebService {
-	
+
 	private StoreService storeService = ApplicationContext.getStoreService();
 
-	public String getStock(String category, String item) {
+	@GetMapping(path="api/store/{cat}/{item}", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String getStock(@PathVariable("cat") String category, @PathVariable("item") String item) {
 		return "Stock: " + storeService.getStock(category, item);
 	}
-	
-	
+
 
 }
