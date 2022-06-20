535674177720374c2ddf86852a723c16390a839d
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-20 16:08:00 +0200

    play around with RequestHeader and RequestParam

--- org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/store/StoreWebService.java
+++ org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/store/StoreWebService.java
@@ -4,7 +4,8 @@ import org.javacream.store.StoreService;
 import org.javacream.util.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +13,14 @@ public class StoreWebService {
 
 	private StoreService storeService = ApplicationContext.getStoreService();
 
-	@GetMapping(path="api/store/{cat}/{item}", produces = MediaType.TEXT_PLAIN_VALUE)
-	public String getStock(@PathVariable("cat") String category, @PathVariable("item") String item) {
+//	@GetMapping(path="api/store/{cat}/{item}", produces = MediaType.TEXT_PLAIN_VALUE)
+//	public String getStock(@PathVariable("cat") String category, @PathVariable("item") String item) {
+//		return "Stock: " + storeService.getStock(category, item);
+//	}
+
+	@GetMapping(path="api/store", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String getStock(@RequestHeader("cat") String category, @RequestParam("item") String item) {
 		return "Stock: " + storeService.getStock(category, item);
 	}
-
 
 }
