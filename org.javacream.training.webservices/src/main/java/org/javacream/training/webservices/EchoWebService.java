eaba410ad58e877c4ec758fadcb9429fe3fc858c
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-20 12:04:47 +0200

    first contact: add mapping information

--- org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/EchoWebService.java
+++ org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/EchoWebService.java
@@ -1,7 +1,12 @@
 package org.javacream.training.webservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EchoWebService {
 
+	@GetMapping(path = "ping")
 	public String ping() {
 		return "pong";
 	}
