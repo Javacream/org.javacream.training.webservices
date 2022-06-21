0be3d043b254426677c99ba06d40296d51d6685b
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-21 11:58:53 +0200

    implement CRD

--- org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/books/BooksWebService.java
+++ org.javacream.training.webservices/src/main/java/org/javacream/training/webservices/books/BooksWebService.java
@@ -8,7 +8,11 @@ import org.javacream.books.warehouse.BooksService;
 import org.javacream.books.warehouse.value.Book;
 import org.javacream.util.ApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,11 +33,23 @@ public class BooksWebService {
 		return booksService.findAllBooks().stream().map((book) -> new BookInfo(book.getIsbn(), book.getTitle())).collect(Collectors.toList());
 	}
 
+	@PostMapping(path = "api/books", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String newBook(@RequestHeader("title") String title) throws BookException {
+		return booksService.newBook(title);
+	}
+
+	@GetMapping(path="api/books/{isbn}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Book findBookByIsbn(@PathVariable("isbn") String isbn) throws BookException {
+		return booksService.findBookByIsbn(isbn);
+	}
+
+	@DeleteMapping(path="api/books/{isbn}")
+	public void deleteBookByIsbn(@PathVariable("isbn") String isbn) throws BookException {
+		booksService.deleteBookByIsbn(isbn);
+	}
+
 	
 //später...	
-	public void deleteBookByIsbn(String arg0) throws BookException {
-		booksService.deleteBookByIsbn(arg0);
-	}
 
 
 	public Book findBook(String arg0, boolean arg1) throws BookException {
@@ -44,13 +60,7 @@ public class BooksWebService {
 		return booksService.findBook(arg0);
 	}
 
-	public Book findBookByIsbn(String arg0) throws BookException {
-		return booksService.findBookByIsbn(arg0);
-	}
 
-	public String newBook(String arg0) throws BookException {
-		return booksService.newBook(arg0);
-	}
 
 	public Book updateBook(Book arg0) throws BookException {
 		return booksService.updateBook(arg0);
